824db5f1240ecbba4fc2cdbe438a3832f5dbcad2
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-11-03 21:26:08 +0100

    Add files via upload

---
+++ Expenditure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueteRazor.Models
+{
+    public class Expenditure
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+
+        public Expenditure (string name, DateTime date, double price)
+        {
+            Name = name;
+            Date = date;
+            Price = price; 
+        }
+
+
+    }
+}
---
+++ ExpenditureController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QueteRazor.Models;
+
+namespace QueteRazor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        // GET: Expenditure
+        public ActionResult CreateList()
+        {
+            List<Expenditure> listeDepense = new List<Expenditure>();
+            listeDepense.Add(new Expenditure ("St Jacques", new DateTime(2021,2,8), 15)) ;
+            listeDepense.Add(new Expenditure("Munster", new DateTime(2021, 2, 8), 3.5));
+            listeDepense.Add(new Expenditure("Avocat", new DateTime(2021, 2, 8), 1.5));
+            ViewData["somme"] = listeDepense.Select(e => e.Price).Sum(); 
+            return View("CreateList", listeDepense);
+        }
+    }
+}
